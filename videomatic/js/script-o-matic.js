ed6da227f39094dfe49ebd41746f054806fadfab
Patrice Ehlert <ehlertpm@users.noreply.github.com>
2017-07-14 10:58:51 -0700

    added images/jquery to switch form action

--- videomatic/js/script-o-matic.js
+++ videomatic/js/script-o-matic.js
@@ -1,3 +1,23 @@
     $('body').on('change keyup keydown paste cut', 'textarea', function() {
       $(this).height(0).height(this.scrollHeight);
     }).find('textarea').change();
+
+    $('input.male-doctor').on('change', function() {
+      $('.male-doctors').show();
+      $('.female-doctors').hide();
+    });
+
+    $('input.female-doctor').on('change', function() {
+      $('.female-doctors').show();
+      $('.male-doctors').hide();
+    });
+
+    $('input.female-patient').on('change', function() {
+      $('.female-patients').show();
+      $('.male-patients').hide();
+    });
+
+    $('input.male-patient').on('change', function() {
+      $('.male-patients').show();
+      $('.female-patients').hide();
+    });
